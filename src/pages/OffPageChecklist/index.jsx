8e9dd4b3ec04d2e1cec79b78c35d7f83746e2c99
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-04 21:58:02 +0530

    feat: added display off-page checklist feature

--- src/pages/OffPageChecklist/index.jsx
+++ src/pages/OffPageChecklist/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Checklist from "../../components/Checklist";
+import Nav from "../../components/Nav";
+import {offPageChecklist} from '../../data/offpageSEO'
 
 const OffPageChecklist = () => {
-  return <div>OffPageChecklist</div>;
+  return <div>
+    <Nav/>
+    <Checklist title = "Off-page Checklist" checklistData = {offPageChecklist}/>
+  </div>;
 };
 
 export default OffPageChecklist;
