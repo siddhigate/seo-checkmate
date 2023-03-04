06f5dc24797f0f32150149964d61ca04238dda26
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-03-04 20:38:36 +0530

    feat: home page skeleton

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div className="landing-container">
+      <h1>SEO Checkmate</h1>
+      <p>Win the SEO game with SEO Checkmate by your side!</p>
+      <Link to="/home">Get Started</Link>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
