795525d30d9902bfec4e2b0f5c3528f41c4c08e5
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-03-05 09:28:08 +0530

    style: checklist page

--- src/components/Checklist/index.jsx
+++ src/components/Checklist/index.jsx
@@ -6,11 +6,13 @@ const Checklist = ({ title, checklistData }) => {
   return (
     <div className="main-container">
       <h1>{title}</h1>
+      <div className="underline"></div>
 
       {Object.keys(checklistData).map((key) => {
         return (
           <div className="list-element" key = {Math.random()}>
             <h2>{key}</h2>
+            <div className="sub-heading-underline"></div>
             {checklistData[key].map(item => {
               return (
                 <div className="list-card" key = {Math.random()}>
